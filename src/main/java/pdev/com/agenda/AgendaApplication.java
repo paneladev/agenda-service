056ec20f0290d17ab39f79ed07238e5d271dbf0e
Panela Dev <paneladev@gmail.com>
2022-03-04 13:59:58 -0300

    commit inicial do projeto

---
+++ src/main/java/pdev/com/agenda/AgendaApplication.java
@@ -0,0 +1,13 @@
+package pdev.com.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pdev/com/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package pdev.com.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
