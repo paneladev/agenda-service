8794c94899813bcc4ff93c23818887a1342bb6bd
Panela Dev <paneladev@gmail.com>
2022-03-16 08:02:47 -0300

    adicionando swagger a nossa aplicacao

---
+++ src/main/java/pdev/com/agenda/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package pdev.com.agenda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pdev.com.agenda.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("Aplicação de Agendamento de Consultas")
+                .version("1.0.0")
+                .contact(new Contact("Renan Lessa", "http://github.com/paneladev", "paneladev@gmail.com"))
+                .build();
+    }
+}
